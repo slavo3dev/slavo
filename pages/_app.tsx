83d6126d7bb44bfb07d60c5544170e237d4161af
slavo3dev <slavo@slavo3.com>
2022-09-10 09:14:34 -0700

    adding router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { Layout, HeadBasePage, MainNavigation } from "../components"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ( { Component, pageProps }: AppProps )
+{
+    
+    const router = useRouter();
+  
+	useEffect(() => {
+		const handleRouteChange = (url: any) => {
+			ga.pageview(url);
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+    }, [ router.events ] );
+    
     return (
         <Layout>
             <HeadBasePage
