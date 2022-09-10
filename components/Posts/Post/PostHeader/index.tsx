1f0966f47b7f8cea5278e19a581857de78bbddf9
slavo3dev <slavo@slavo3.com>
2022-09-10 12:06:18 -0700

    Post blog page

--- components/Posts/Post/PostHeader/index.tsx
+++ components/Posts/Post/PostHeader/index.tsx
@@ -6,9 +6,9 @@ import { PostHeaderType } from "Types/PostType";
 export const PostHeader: FC<PostHeaderType> = ({ title, imgSrc}) =>
 {
     return (
-      <header className="header">
+        <header className={classes.postheader}>
             <h1>{title}</h1> 
-            <Image src={ imgSrc } alt='title' width={ 200 } height={150} layout="responsive" />
+            <Image src={ imgSrc } alt={title} width={ 200 } height={150}  />
       </header>
   )
 }
