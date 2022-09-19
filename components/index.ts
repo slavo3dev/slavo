71a74cd043e2da38fef76a7591d04ab4ba2453b2
slavo3dev <slavo@slavo3.com>
2022-09-19 18:57:40 -0400

    missing semicolon

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,11 @@
 import { Layout } from "./Layout/layout";
 import { MainNavigation } from "./Layout/MainNavigation";
 import { Hero } from "./Hero";
-import { FeaturedPosts } from "./Posts"
+import { FeaturedPosts } from "./Posts";
 import { HeadBasePage } from "./HeadBasePage";
 import { BlogPosts } from "./Posts/BlogPosts";
 import { PostContent, PostHeader } from "./Posts/Post";
 import { ContactForm } from "./ContactForm";
 import { Subscribe } from "./Subscribe";
 
-export {Layout, Hero, FeaturedPosts, HeadBasePage, MainNavigation, BlogPosts, PostContent, PostHeader, ContactForm, Subscribe}
+export {Layout, Hero, FeaturedPosts, HeadBasePage, MainNavigation, BlogPosts, PostContent, PostHeader, ContactForm, Subscribe};
