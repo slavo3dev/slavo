f67884cf6c10f08df9218983007966b680c08cac
slavo3dev <slavo@slavo3.com>
2022-09-19 18:19:37 -0400

    fixing semicolons

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,35 +1,36 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import { PostContent } from "@/components/index";
 import { getPostData, getPostsFiles } from "@/lib/posts-lib";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Post: NextPage = ( { post }: any ) =>
 {
-    return <PostContent post={post} />
+	return <PostContent post={post} />;
 };
 
 export function getStaticProps ( context: { params :{ slug: string} } )
 {
-    const { slug } = context.params
+	const { slug } = context.params;
 
-    const postData = getPostData(slug) 
+	const postData = getPostData(slug); 
 
-    return {
-        props: {
-            post: postData
-        },
-        revalidate: 600
-    };
+	return {
+		props: {
+			post: postData
+		},
+		revalidate: 600
+	};
 }
 
 export function getStaticPaths ()
 {
-    const postName = getPostsFiles()
+	const postName = getPostsFiles();
 
-    const slugs = postName.map(fileName => fileName.replace(/\.md$/, ''))
-    return {
-        paths: slugs.map( slug => ( { params: {slug: slug} } )),
-        fallback: true
-   }    
+	const slugs = postName.map(fileName => fileName.replace(/\.md$/, ""));
+	return {
+		paths: slugs.map( slug => ( { params: {slug: slug} } )),
+		fallback: true
+	};    
 }
 
 export default Post;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,22 +1,23 @@
-import type { NextPage } from 'next'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { NextPage } from "next";
 import { BlogPosts } from "@/components/index";
 import { getAllPosts } from "@/lib/posts-lib";
 
 const blog: NextPage = ( { posts }: any ) =>
 {
-    return <BlogPosts posts={posts} />
+	return <BlogPosts posts={posts} />;
 };
 
 
 export function getStaticProps() {
-    const featuredPost = getAllPosts() 
+	const featuredPost = getAllPosts(); 
 
-    return {
-        props: {
-            posts: featuredPost
-        },
-        revalidate: 60
-    };
+	return {
+		props: {
+			posts: featuredPost
+		},
+		revalidate: 60
+	};
 }
 
 export default blog;
