01bc5d57fff5ff0b11b8d17c0bc3ac8265a82480
slavo3dev <slavo@slavo.io>
2023-01-16 18:23:02 -0500

    removing console.log

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPPABASE_URL || "";
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPPABASE_KEY || "";
 
-console.log("supabaseUrl: ", supabaseUrl);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
