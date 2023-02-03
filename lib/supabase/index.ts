a1aaa83cfa2f534ce860240333c2386cca9c17b7
slavo3dev <slavo@slavo.io>
2023-02-03 13:11:21 -0500

    fixing path to supa base

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPPABASE_URL || "";
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPPABASE_KEY || "";
