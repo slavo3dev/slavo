93214585c0c9cb261dec160f9f82249f1ce15da0
slavo3dev <slavo@slavo.io>
2023-02-03 17:45:11 -0500

    fixes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,13 +1,29 @@
 import { FC } from "react";
+import { useEffect, useState} from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import classes from "./hero.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-export const Hero: FC = () =>
-{
+export const Hero: FC = () => {
+	const { user } = useUser();
+	const [ userEmail, setUserEmail ] = useState<string | undefined | null>("");
+	const [ userVerified, setUserVerified ] = useState<boolean | null>();
     
-	// eslint-disable-next-line no-constant-condition
-	const isAuthTitle = false ? "Login" : "Create Account";
+	useEffect( () => {
+		if ( user )
+		{
+			setUserEmail( user.email );
+			setUserVerified(user?.email_verified);
+		}
+	},[userEmail, userVerified, user]);
+
+	const isAuth = (userEmail && userVerified);
+	console.log( isAuth );
+	console.log("User: ", user);
+	console.log( "userEmail: ", userEmail );
+	console.log( "userVerify: ", userVerified );
+    
 	return (
 		<section className={ classes.hero }>
 			<div className={classes.imagehero}>
@@ -21,7 +37,7 @@ export const Hero: FC = () =>
 			</div>
 			<h1>Slavo</h1>
 			<p className={classes.quote}>The future depends on what<br /> you do today.</p>
-			<Link href="/api/auth/login"><button className="hover:bg-blue-100 bg-blue-500 text-white hover:text-red-500 font-bold py-2 px-4 rounded">{isAuthTitle}</button></Link>
+			<Link href={isAuth ? "/freesource" : "/api/auth/login"}><button className="hover:bg-blue-100 bg-blue-500 text-white hover:text-red-500 font-bold py-2 px-4 rounded">{isAuth ? "Free Sources" : "Login"}</button></Link>
 		</section>
 	);
 };
--- components/Layout/MainNavigation/index.tsx
+++ components/Layout/MainNavigation/index.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 import classes from "./navigation.module.css";
 import { Logo } from "../Logo";
 import { FC } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 
 export const MainNavigation: FC = () =>
 {
-
-	const isAuth = false;
+	const { user } = useUser();
+	const isAuth = user?.email;
+    
+	const auth_terms = "Please log in/Verify your e-mail\nTo access the Free Sources Page.";
 	return (
 		<header className={classes.header}>
 			<Link href="/">
@@ -21,16 +24,16 @@ export const MainNavigation: FC = () =>
 					<li>
 						<Link href="/blog">Blog</Link>
 					</li>
-					<li>
-						<Link href="/freesource">ReSource</Link>
-					</li>
+					{ isAuth ? <li><Link href="/freesource">ReSource</Link></li> : <li onClick={() => alert(auth_terms)}><Link href="/">ReSource</Link></li> }
 					<li>
 						<Link href="/contact">Contact</Link>
 					</li>
+
+					{ isAuth && <li><Link href="/api/auth/logout">Logout</Link></li> }
+					
 					<li>
-						<Link href="/api/auth/login">Login</Link>
+						{ isAuth ? <Link href="/freesource">{user.email}</Link> : <Link href="/api/auth/login">Login</Link>}
 					</li>
-					{ isAuth && <li><Link href="/contact">Logout</Link></li> }
 				</ul>
 			</nav>
 		</header>
