97b4c0e59cc4fdea0c35ed61525aa22557fd5006
slavo3dev <slavo@slavo.io>
2023-02-03 16:20:04 -0500

    tets

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,54 @@
 /* eslint-disable indent */
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+const safelist = [
+  /html/,
+  /body/,
+  /^-tw-/,
+  /^tw-/,
+  /^maxSm:/,
+  /^maxXl:/,
+  /^maxLg:/,
+  /^smToMd:/,
+  /^sm:/,
+  /^md:/,
+  /^lg:/,
+  /^xl:/,
+  /^2xl:/,
+  /^3xl:/,
+  /^child:/,
+  /^hover:/,
+  /^focus:/,
+  /^group/,
+  /^group-hover:tw-/,
+  /^last:/,
+  /^first:/,
+  /^even:/,
+  /^before:/,
+  /^after:/,
+  /^nextIcon^/,
+  /^modal-/,
+  /^swiper/,
+  /^react-tabs/,
+];
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    process.env.NODE_ENV === "dev"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: ["./src/**/*.tsx"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: {
+              standard: safelist,
+              deep: safelist,
+              greedy: safelist,
+            },
+          },
+        ]
+      : null,
+  ],
 };
