fcb112a83a391df9d27fafa9aa0668979f191638
Danilo Kulisic <danilokulisic4@gmail.com>
2023-04-04 23:36:53 +0200

    This commit will change h2 to Title component

--- components/Posts/FeaturedPosts/index.tsx
+++ components/Posts/FeaturedPosts/index.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 import classes from "./FeaturedPosts.module.css";
 import { PostsGrid } from "../PostsGrid";
 import { PostsList } from "Types/PostsList";
-
+import { Title } from "../../Title";
 
 export const FeaturedPosts: FC<PostsList> = ({ posts }) =>
 {
 	return (
 		<div className={classes.latest}>
-			<h2>Featured Posts </h2> 
+			<Title title={"Featured Posts"} /> 
 			<PostsGrid posts={posts} /> 
 		</div>
 	);
