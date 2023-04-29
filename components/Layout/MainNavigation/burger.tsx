fdebff2a345dcb9d29a0a3c89a6228914679a30a
nikolaklenje <nikola@prototypenext.com>
2023-04-29 09:07:50 -0400

    scroll

--- components/Layout/MainNavigation/burger.tsx
+++ components/Layout/MainNavigation/burger.tsx
@@ -12,17 +12,20 @@ export const Burger : FC = ()=>{
 		setShowDrop(!showDrop);
 	};
 
-	return <div className="sm:hidden">
+	return <div className="sm:hidden flex-row overflow-show">
 
 	
-		<button onClick={handleBurger}className="navbar-burger flex items-center py-2 px-3 mt-4 mr-4 text-blue-500 hover:text-blue-700 rounded border border-blue-200 hover:border-blue-300">
+		<button onClick={handleBurger}className=" z-50 right-4 items-center py-2 px-3 mt-4 mr-4 text-blue-500 hover:text-blue-700 rounded border border-blue-200 hover:border-blue-300">
 			<svg className="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
 		</button>
 			
-		<div className={showDrop ? "visible overflow-scroll" : "hidden"}>
-			<div className="absolute flex-col bg-white  left-0 z-40 mt-2 w-full rounded border-[.5px] border-light px-5 py-5 transition-all">
-				<ul className="navbar-nav mr-auto w-full flex list-none flex-col z-50">
+		<div className={showDrop ? "visible " : "hidden"}>
+
+
+			<div className="fixed flex-col h-5/6 overflow-scroll bg-white left-0 z-40 mt-2 w-full rounded border-[.5px] border-light px-5 py-5 transition-all">
+
+				<ul className="overflow-y-scroll  mr-auto w-full flex list-none flex-col ">
 					<li className="w-11/12 p-4 text-sm text-gray-500 hover:text-blue-500 hover:bg-blue-50 rounded-xl">Home</li>
 					<li className="w-11/12 p-4 text-sm text-gray-500 hover:text-blue-500 hover:bg-blue-50 rounded-xl">About Us</li>
 					<li className="w-11/12 p-4 text-sm text-gray-500 hover:text-blue-500 hover:bg-blue-50 rounded-xl">Services</li>
@@ -33,22 +36,22 @@ export const Burger : FC = ()=>{
 					<li className="w-11/12 p-4 text-sm text-gray-500 hover:text-blue-500 hover:bg-blue-50 rounded-xl">Faqs</li>
 					<li className="w-11/12 p-4 text-sm text-gray-500 hover:text-blue-500 hover:bg-blue-50 rounded-xl">Contact Us</li>
 				</ul>
+
 				<div className="w-full mt-4 pt-6 border-t border-blueGray-50 flex flex-col">
 					<div className="w-11/12 px-4 py-3 mb-3 text-xs text-center no-underline font-semibold rounded-xl bg-blue-400 hover:bg-blue-500 text-white rounded-xl">
 						<Link className="" href={""}>SignUp</Link></div>
 					<div className="w-11/12 px-4 py-3 mb-3 text-xs text-center font-semibold rounded-xl text-blue-500 hover:text-blue-700 border border-blue-200 hover:border-blue-300 rounded"><Link href={""}>Login</Link></div>
 				</div>
+
 				<div>Contact us example@example.com</div>
+
+
 				<div>
 					
 					<Image src={insta}></Image>		
 					<Image src={twit}></Image>	
 					<Image src={face}></Image>		
 				</div>
-				
-				
-				
-				
 			</div>
 		</div>
 		
