c2c2dbfc1741560c5eff2c6117b21e5dab0064fa
slavo3dev <slavo@slavo.io>
2023-05-02 11:12:34 -0400

    adding loader and removing title

--- components/Posts/BlogPosts/index.tsx
+++ components/Posts/BlogPosts/index.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 import classes from "./blogposts.module.css";
 import { PostsGrid } from "../PostsGrid";
 import { PostsList } from "Types/PostsList";
+import { Loader } from "@/components/ui/Loader";
 
 export const BlogPosts: FC<PostsList> = ({ posts }) =>
 {
 	return (
 		<section className={ classes.allposts }>
-			<h1>Articles</h1>
-			<PostsGrid posts={posts} />
+			{ posts ? <PostsGrid posts={posts} /> : <Loader title="We are Loading Posts" />}
 		</section>
 	);
 };
