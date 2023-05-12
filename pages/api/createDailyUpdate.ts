be20a00e15f53fc1e3cbd4f7754c5e59f0b06479
slavo3dev <slavo@slavo.io>
2023-05-12 19:35:59 -0400

    updating and moving calls to backend

--- pages/api/createDailyUpdate.ts
+++ pages/api/createDailyUpdate.ts
@@ -12,17 +12,33 @@ export default async function handler(
 	res: NextApiResponse<Data>
 )
 {
-	try {
-		const { data: newUpdate, error } = await supabase
-			.from("porch")
-			.insert([req.body])
-			.select();
-        
-		res.status( 200 ).json(
-			{
+	try
+	{
+		if ( req.method === "PATCH" )
+		{
+			const { data, error } = await supabase
+				.from("porch")
+				.update({ "excellent": req.body.vote })
+				.eq("id", req.body.id)
+				.select();
+            
+			res.status( 200 ).json({
+				ticket: "Thank you for support!!",
+				newUpdate: data
+			} );
+		} else
+		{
+			const { data: newUpdate, error } = await supabase
+				.from("porch")
+				.insert([req.body])
+				.select();
+            
+			res.status( 200 ).json({
 				ticket: "Well Done - Your Daily Update is ADDED -",
 				newUpdate: newUpdate
 			} );
+		}
+        
 	} catch (error) {
 		throw new Error("Oops, Source was not created, please try again");
         
