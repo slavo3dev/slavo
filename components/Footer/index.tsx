498d2784b58b66aa6a4ede306cd0ce2214de2605
nikolaklenje <nikola@prototypenext.com>
2023-05-15 16:33:32 -0400

    Removed playlist data

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 export const Footer:FC = () => {
 	return (
--- pages/videos.tsx
+++ pages/videos.tsx
@@ -13,14 +13,14 @@ const Videos: NextPage = () => {
 	return (
 		<div className="p-8">
 			<div>
-				<h1 className="text-3xl  ml-6 font-bold font-family-jakarta text-black">Free Code Camp</h1>
+				<h1 className="text-3xl  ml-6 font-bold font-family-jakarta text-black">Udacity</h1>
 				<div className="overflow-scroll overflow-y-hidden"
 					onClick={() => context.setVideoLine("channelOne")}>
 					{udacityVideos}
 				</div>
 			</div>
 			<div>
-				<h1 className="text-3xl  ml-6 font-bold font-family-jakarta text-black">Udacity</h1>
+				<h1 className="text-3xl  ml-6 font-bold font-family-jakarta text-black">Free Code Camp</h1>
 				<div
 					className="overflow-scroll overflow-y-hidden"
 					onClick={() => context.setVideoLine("channelTwo")}>
