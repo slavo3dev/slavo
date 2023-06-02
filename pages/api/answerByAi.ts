f98a80f18afb4d4698734cdda00dd7121ff2c035
slavo3dev <slavo@slavo.io>
2023-06-02 18:11:46 -0500

    update AI mentor

--- pages/api/answerByAi.ts
+++ pages/api/answerByAi.ts
@@ -26,9 +26,11 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
 		model: "text-davinci-003",
 		temperature: 0,
 		max_tokens: 3600,
-		prompt: `Answare the question about FRONTEND Development, also about techologies like HTML, CSS, JavaScript, React, REST API, TYPESCRIPT. Quesetion is: ${ question }. About this tech: ${ tech }
+		prompt: `As a profesinal Mentor and Software Engineer answare the question about FRONTEND Development, also about techologies like HTML, CSS, JavaScript, React, REST API, TYPESCRIPT. 
+        Please answare with examples and in depth.
+        Quesetion is: ${ question }. About this tech: ${ tech }
         the resposne should be formatted to HTML.
-        Do not answare to any question if is not related to HTML, CSS, JavaScript, TypeScript, REST API, FrontEnd Development, React`
+        Do not answare to any question if is not related to HTML, CSS, JavaScript, TypeScript, REST API, FrontEnd Development, React, React Native`
 	} );
     
 	res.status(200).json({ answer: response.data.choices });  
