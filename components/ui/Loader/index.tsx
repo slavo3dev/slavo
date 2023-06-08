97feaa373e0beaef30aeedde08c3adf2039cdbd8
slavo3dev <slavo@slavo.io>
2023-06-08 16:17:24 -0400

    update color btn

--- components/ui/Loader/index.tsx
+++ components/ui/Loader/index.tsx
@@ -9,10 +9,10 @@ export const Loader: FC<LoaderProp> = ( { title = "Loading..." } ) =>  {
 	return (
 		<div className="flex items-center justify-center p-10 flex-col">
 			<div className="flex items-center justify-center p-8">
-				<div className="spinner-border rounded bg-blue-500 animate-spin inline-block w-16 h-16 border-6 text-black" role="status">
+				<div className="spinner-border rounded bg-blue-600 animate-spin inline-block w-16 h-16 border-6 text-black" role="status">
 				</div>
 			</div>
-			<p className= "text-sky-800 text-lg animate-ping p-4">{title}</p>
+			<p className= "text-sky-700 text-lg animate-ping p-4">{title}</p>
 		</div>
 	);
 };
