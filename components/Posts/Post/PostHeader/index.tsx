0865287b33157ea5b609277232a9aedc0dd95007
slavo3dev <slavo@slavo.io>
2023-07-04 15:57:32 -0500

    improving for seo

--- components/Posts/Post/PostHeader/index.tsx
+++ components/Posts/Post/PostHeader/index.tsx
@@ -2,13 +2,17 @@ import { FC } from "react";
 import Image from "next/image";
 import classes from "./postheader.module.css";
 import { PostHeaderType } from "Types/PostType";
+import { HeadBasePage } from "@/components/HeadBasePage";
 
 export const PostHeader: FC<PostHeaderType> = ({ title, imgSrc}) =>
 {
 	return (
-		<header className={classes.postheader}>
-			<h1>{title}</h1> 
-			<Image src={ imgSrc } alt={title} width={ 200 } height={150}  />
-		</header>
+		<>
+			<HeadBasePage title={title} />
+			<header className={classes.postheader}>
+				<h1>{title}</h1> 
+				<Image src={ imgSrc } alt={title} width={ 200 } height={150}  />
+			</header>
+		</>
 	);
 };
