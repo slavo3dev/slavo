78408a84f7d5b8557c84ed56b9a8523c3b17baf1
Danilo Kulisic <danilokulisic4@gmail.com>
2023-08-30 14:50:37 +0200

    Changed styles to shadow of parent div, label, select, placeholder of textarea, and button element

--- components/OpenAI/index.tsx
+++ components/OpenAI/index.tsx
@@ -39,13 +39,15 @@ export const OpenAI: FC = () => {
 	return (
 		<>
 			<div className="lg:flex rounded shadow-lg lg:max-w-full sm:max-w-full md:max-w-full" id="openai">
-				<div className="bg-white shadow-md shadow-sky-700 rounded px-8 pt-6 pb-8 w-full lg:max-w-full">
+				<div className="bg-white shadow-lg shadow-sky-400 rounded px-8 pt-6 pb-8 w-full lg:max-w-full">
 					<div className="mb-7 pl-8 pr-8 flex pb-4 flex-col">
-						<label className="block text-gray-700 lg:text-xl md:text-xl font-bold mb-2 lg:text-left md:text-center sm:text-xs" >
-                            Ask Question About:<br />* Choose: [ "HTML | CSS | JavaScript | TypeScript | React | NodeJS | API" ]<br />* Suggestions, Advice & Solutions
+						<label className="block text-gray-700 first-letter:text-7xl first-letter:font-bold first-letter:text-blue-600 lg:text-3xl md:text-xl font-bold mb-2 lg:text-left md:text-center sm:text-xs" >
+                            Ask Question About:
+							<p className="text-gray-700 lg:text-xl font-normal mt-7">HTML | CSS | JavaScript | TypeScript | React | NodeJS | API</p>
+							<p className="text-gray-700 lg:text-xl font-normal mb-7">Suggestions, Advice & Solutions</p>
 						</label>
 						<select
-							className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-3"
+							className="block w-1/6 bg-white border-2 border-white border-b-blue-600 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-white mb-3"
 							onChange={ e => setTech( e.target.value ) }
 							value={ tech } > 
 							<option value="HTML">HTML</option>
@@ -58,13 +60,13 @@ export const OpenAI: FC = () => {
 						</select>
 						<textarea
 							id="editor" rows={ 8 }
-							className="shadow appearance-none block w-full py-3 px-3 text-sm text-gray-800 bg-whitedark:bg-gray-800 focus:ring-0 dark:text-bule dark:placeholder-gray-400 border rounded focus:outline-none focus:shadow-outline"
+							className="shadow appearance-none block w-full py-3 px-3 text-sm text-gray-800 bg-whitedark:bg-gray-800 focus:ring-0 dark:text-bule dark:placeholder-gray-400 border rounded focus:outline-none focus:shadow-outline placeholder:italic placeholder:text-slate-400"
 							placeholder="Ask Question FronEnd Tech - HTML, CSS, JavaScript..."
 							required
 							value={ questionInput }
 							onChange={ e => setQuestionInput( e.target.value ) } />
 						<button
-							className="text-sm font-medium text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800 mt-4 p-6 md:max-w-full "
+							className="text-sm font-medium text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800 mt-4 p-3 lg:w-1/6 md:max-w-full "
 							style={{ textAlign: "center"}}
 							type="button"
 							disabled={isLoading}
