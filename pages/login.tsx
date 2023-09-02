cd4f644e66fe255e4777ff016da9c0ef813652e2
nikolaklenje <nikola@prototypenext.com>
2023-09-01 22:23:42 -0400

    Check are passwords matching for new user

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,21 +15,9 @@ const Login: NextPage = () => {
 
   const [userEmail, setUserEmail] = useState<string>("");
   const [userPassword, setUserPassword] = useState<string>("");
+  const [matchingPassword, setMatchingPassword] =
+    useState<string>("");
   const [signUpError, setSignUpError] = useState<string>("");
-
-  // const signInWithEmail: () => void = async () => {
-  //   await supabase.auth.signInWithPassword({
-  //     email: userEmail,
-  //     password: userPassword,
-  //   });
-  //   router.push("/");
-  //   console.log("Request is Ok");
-  // };
-
-  // const { data, error } = await supabase.auth.signUp({
-  //   email: userEmail,
-  //   password: userPassword,
-  // });
 
   const signUpWithEmail: () => void = async () => {
     try {
@@ -112,24 +100,39 @@ const Login: NextPage = () => {
                         placeholder="••••••••"
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
-                      {signUpError}
                     </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Confirm Password
+                      </label>
+                      <input
+                        onChange={(e) => {
+                          setMatchingPassword(e.target.value);
+                        }}
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                    </div>
+                    {signUpError}
                     <div className="flex items-center justify-between">
                       <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input
-                            id="remember"
-                            aria-describedby="remember"
-                            type="checkbox"
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                          />{" "}
-                        </div>
+                        <div className="flex items-center h-5"></div>
                       </div>
                     </div>
                     <button
                       onClick={(e) => {
                         e.preventDefault();
-                        signUpWithEmail();
+                        if (matchingPassword == userPassword) {
+                          signUpWithEmail();
+                        } else {
+                          setSignUpError("Passwords Not Matching");
+                        }
                       }}
                       type="submit"
                       className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
