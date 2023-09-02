fbf0a402d99eb5f9c64141645c28387e105cc948
nikolaklenje <nikola@prototypenext.com>
2023-09-01 22:07:33 -0400

    Added sign up function

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,13 +15,38 @@ const Login: NextPage = () => {
 
   const [userEmail, setUserEmail] = useState<string>("");
   const [userPassword, setUserPassword] = useState<string>("");
+  const [signUpError, setSignUpError] = useState<string>("");
 
-  const signInWithEmail: () => void = async () => {
-    await supabase.auth.signInWithPassword({
-      email: userEmail,
-      password: userPassword,
-    });
-    router.push("/");
+  // const signInWithEmail: () => void = async () => {
+  //   await supabase.auth.signInWithPassword({
+  //     email: userEmail,
+  //     password: userPassword,
+  //   });
+  //   router.push("/");
+  //   console.log("Request is Ok");
+  // };
+
+  // const { data, error } = await supabase.auth.signUp({
+  //   email: userEmail,
+  //   password: userPassword,
+  // });
+
+  const signUpWithEmail: () => void = async () => {
+    try {
+      const { error } = await supabase.auth.signUp({
+        email: userEmail,
+        password: userPassword,
+      });
+      if (error) {
+        console.log("Signup error:", error.message);
+        setSignUpError(error.message);
+      } else {
+        router.push("/");
+        console.log("Request is ok");
+      }
+    } catch (error) {
+      console.log("Error during signup:", error);
+    }
   };
 
   useEffect(() => {
@@ -49,7 +74,7 @@ const Login: NextPage = () => {
               <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                    Sign up to your account
                   </h1>
                   <form className="space-y-4 md:space-y-6" action="#">
                     <div>
@@ -87,6 +112,7 @@ const Login: NextPage = () => {
                         placeholder="••••••••"
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
+                      {signUpError}
                     </div>
                     <div className="flex items-center justify-between">
                       <div className="flex items-start">
@@ -98,30 +124,17 @@ const Login: NextPage = () => {
                             className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
                           />{" "}
                         </div>
-                        <div className="ml-3 text-sm">
-                          <label
-                            htmlFor="remember"
-                            className="text-gray-500 dark:text-gray-300"
-                          >
-                            Remember me
-                          </label>
-                        </div>
                       </div>
-                      <a
-                        href="#"
-                        className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      >
-                        Forgot password?
-                      </a>
                     </div>
                     <button
-                      onSubmit={() => {
-                        signInWithEmail;
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signUpWithEmail();
                       }}
                       type="submit"
                       className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     >
-                      Sign in
+                      Sign up
                     </button>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet?{" "}
