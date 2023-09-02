e5bc10c62dbd6c45240ad25986160d4c83b5de3d
nikolaklenje <nikola@prototypenext.com>
2023-09-02 00:17:04 -0400

    Adding sign In function

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,8 +13,8 @@ const Login: NextPage = () => {
 
   const [userInfo, setUserInfo] = useState<any>(null);
 
-  const [userEmail, setUserEmail] = useState<string>("");
-  const [userPassword, setUserPassword] = useState<string>("");
+  const [newUserEmail, setNewUserEmail] = useState<string>("");
+  const [newUserPassword, setNewUserPassword] = useState<string>("");
   const [matchingPassword, setMatchingPassword] =
     useState<string>("");
   const [signUpError, setSignUpError] = useState<string>("");
@@ -22,21 +22,22 @@ const Login: NextPage = () => {
   const signUpWithEmail: () => void = async () => {
     try {
       const { error } = await supabase.auth.signUp({
-        email: userEmail,
-        password: userPassword,
+        email: newUserEmail,
+        password: newUserPassword,
       });
       if (error) {
         console.log("Signup error:", error.message);
         setSignUpError(error.message);
-      } else {
-        router.push("/");
-        console.log("Request is ok");
+        return;
       }
-    } catch (error) {
-      console.log("Error during signup:", error);
+      router.push("/");
+      console.log("Request is ok");
+    } catch (err) {
+      console.log("Error during signup:", err);
     }
   };
 
+  // Set state for login name and password, then handle response
   useEffect(() => {
     const fetchData = async () => {
       const userData = await getUserData();
@@ -46,6 +47,23 @@ const Login: NextPage = () => {
     fetchData();
   }, []);
 
+  const signInWithEmail: () => void = async () => {
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: "example@email.com",
+        password: "example-password",
+      });
+      if (error) {
+        console.log("Signup error:", error.message);
+        setSignUpError(error.message);
+        return;
+      }
+      router.push("/");
+      console.log("Request is ok");
+    } catch (error) {
+      console.log("Error during sign in", error);
+    }
+  };
   return (
     <section className="h-screen">
       <div className="container h-full px-6 py-24">
@@ -62,7 +80,7 @@ const Login: NextPage = () => {
               <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign up to your account
+                    Sign up for new account
                   </h1>
                   <form className="space-y-4 md:space-y-6" action="#">
                     <div>
@@ -74,7 +92,7 @@ const Login: NextPage = () => {
                       </label>
                       <input
                         onChange={(e) => {
-                          setUserEmail(e.target.value);
+                          setNewUserEmail(e.target.value);
                         }}
                         type="email"
                         name="email"
@@ -92,7 +110,7 @@ const Login: NextPage = () => {
                       </label>
                       <input
                         onChange={(e) => {
-                          setUserPassword(e.target.value);
+                          setNewUserPassword(e.target.value);
                         }}
                         type="password"
                         name="password"
@@ -128,7 +146,7 @@ const Login: NextPage = () => {
                     <button
                       onClick={(e) => {
                         e.preventDefault();
-                        if (matchingPassword == userPassword) {
+                        if (matchingPassword == newUserPassword) {
                           signUpWithEmail();
                         } else {
                           setSignUpError("Passwords Not Matching");
@@ -139,15 +157,58 @@ const Login: NextPage = () => {
                     >
                       Sign up
                     </button>
-                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet?{" "}
-                      <a
-                        href="#"
-                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      >
-                        Sign up
-                      </a>
+                  </form>
+                  <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-blue-800 after:mt-0.5 after:flex-1 after:border-t after:border-blue-800">
+                    <p className="mx-4 mb-0 text-center font-semibold dark:text-blue-800">
+                      OR
                     </p>
+                  </div>
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Sign in to your account
+                  </h1>
+                  <form className="space-y-4 md:space-y-6" action="#">
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Your email
+                      </label>
+                      <input
+                        onChange={(e) => {
+                          setNewUserEmail(e.target.value);
+                        }}
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@company.com"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Password
+                      </label>
+                      <input
+                        onChange={(e) => {
+                          setNewUserPassword(e.target.value);
+                        }}
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      Sign in
+                    </button>
                   </form>
                 </div>
               </div>
