d3fffd5f04e560cbcbdcf6f00db34d6e8f8b140a
slavo3dev <slavo@slavo.io>
2023-11-22 07:35:36 -0500

    working on re-design

--- components/Posts/FeaturedPosts/index.tsx
+++ components/Posts/FeaturedPosts/index.tsx
@@ -3,13 +3,15 @@ import classes from "./FeaturedPosts.module.css";
 import { PostsGrid } from "../PostsGrid";
 import { PostsList } from "Types/PostsList";
 import { Title } from "../../Title";
+import {  ButtonEvents } from "@/components/Buttons/ButtonEvents";
 
 export const FeaturedPosts: FC<PostsList> = ({ posts }) =>
 {
 	return (
 		<div className={classes.latest}>
 			<Title title={"Featured Posts"} /> 
-			<PostsGrid posts={posts} /> 
+			<PostsGrid posts={ posts } /> 
+			<ButtonEvents link={"/blog"}>See All Articles</ButtonEvents>
 		</div>
 	);
 };
