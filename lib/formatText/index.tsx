8a13fc9254a33c6d68aa13b257413f918d9dd9ad
slavo3dev <slavo@slavo.io>
2023-11-23 15:58:46 -0500

    unmount component

--- lib/formatText/index.tsx
+++ lib/formatText/index.tsx
@@ -5,56 +5,60 @@ import Image from "next/image";
 import classes from "./formatText.module.css";
 
 
-export const customRenderers = () => {
+
+export const CodeBlock = ({ className, children }: { className?: string; children: React.ReactNode }) => {
 	const [isMounted, setIsMounted] = useState(false);
+	// No need to destructure code here, directly use className and children
+	const language = className ? className.split("-")[1] : "javascript";
 
 	useEffect(() => {
 		setIsMounted(true);
 	}, []);
-    
-    
-	return {
-		p(paragraph: any) {
-			const { node } = paragraph;
-			if (node.children[0].tagName === "img" ) {
-				const image = node.children[0];
+
+	// Use children directly instead of code.children
+	return (
+		isMounted ? (
+			<SyntaxHighlighter style={atomDark} language={language} children={children} />
+		) : (
+			<p>Loading...</p> // This can be a placeholder or null if you prefer not to show anything
+		)
+	);
+};
+
+
+export const customRenderers =  {
+	p(paragraph: any) {
+		const { node } = paragraph;
+		if (node.children[0].tagName === "img" ) {
+			const image = node.children[0];
 			
-				return (
-					<div className={classes.image}>
-						<Image
-							src={`${image.properties.src}`}
-							alt={image.alt}
-							width={600}
-							height={300}
-						/>
-					</div>
-				);
-			}
-
-			return <p>{paragraph.children}</p>;
-		},
-
-		code(code: any) {
-			const { className, children } = code;
-			const language = className ? className.split("-")[1] : "js"; // className is something like language-js => We need the "js" part here
 			return (
-				isMounted ? <SyntaxHighlighter
-					style={atomDark}
-					language={language}
-					children={children}
-				/> : <p>Loading..</p>
+				<div className={classes.image}>
+					<Image
+						src={`${image.properties.src}`}
+						alt={image.alt}
+						width={600}
+						height={300}
+					/>
+				</div>
 			);
-		},
-    
-		a ( a: any ) {
-			const urlLink: string = a.node.properties.href;
-			const linkTitle: string = a.node.children[ 0 ].value;
-            
-			return <a
-				className={classes.hoverLink}
-				href={ urlLink } target="_blank">{ linkTitle }
-				<span className={classes.hoverLine}></span>
-			</a>;
 		}
-	};
+
+		return <p>{paragraph.children}</p>;
+	},
+
+	code(code: any) {
+		return <CodeBlock className={code.className} children={code.children} />;	
+	},
+    
+	a ( a: any ) {
+		const urlLink: string = a.node.properties.href;
+		const linkTitle: string = a.node.children[ 0 ].value;
+            
+		return <a
+			className={classes.hoverLink}
+			href={ urlLink } target="_blank">{ linkTitle }
+			<span className={classes.hoverLine}></span>
+		</a>;
+	}
 };
