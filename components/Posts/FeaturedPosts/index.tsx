84b551dc077f34da885cc88a8c4ba61a54f54694
Slavoljub Popovic <18499909+slavo3dev@users.noreply.github.com>
2023-11-24 18:16:09 -0500

    Search feature (#50)
    * improving blogging page
    
    * unmount component
    
    * improving blogging page
    
    * fix: adding category search

--- components/Posts/FeaturedPosts/index.tsx
+++ components/Posts/FeaturedPosts/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FeaturedPostCard } from "./FeaturedPostCard";
 import { PostsList } from "Types/PostsList";
 import {FeaturedTitle} from "./FeaturedTitile";
@@ -8,22 +8,24 @@ import { BlogMenuCat } from "../BlogMenuCat";
 export const FeaturedPosts: FC<PostsList> = ({ posts }) =>
 {
 
-	// const filterPosts = posts.filter( post => post.category.toLowerCase() === "self help");
+	const [ category, setCategory ] = useState( "ALL" );
     
 	const categories: string[] = posts
 		.map(post => post.category) // Extract categories from posts
 		.sort() // Sort the categories alphabetically
 		.filter((item, index, arr) => arr.indexOf(item) === index); // Filter out duplicates
 
-	categories.unshift("ALL");
+	categories.unshift( "ALL" );
+    
+	const filtredPost = category.toLocaleLowerCase() === "all" ? posts : posts.filter(post => post.category.toLocaleLowerCase() === category.toLocaleLowerCase());
 
 	return (
 		<section className="py-12 bg-white sm:py-16 lg:py-20 text-center">
 			<div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl text-center">
 				<FeaturedTitle /> 
-				{/* <BlogMenuCat categories={ categories } />	 */}
+				<BlogMenuCat categories={ categories } onSearch={(cat?: string) => { cat && setCategory(cat);}} />
 				<div className="grid grid-cols-1 gap-12 mt-12 xl:gap-20 sm:grid-cols-2 lg:grid-cols-3 sm:mt-16">
-					{ posts.map( post => <FeaturedPostCard key={post.slug} post={post} />)}
+					{ filtredPost.map( post => <FeaturedPostCard key={post.slug} post={post} />)}
 				</div>
 			</div>
 			<LinkEvents link={ "/blog" } description={"See All Articles"} />
