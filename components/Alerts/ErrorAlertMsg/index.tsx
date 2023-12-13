3f9a3bab377e9e0d4e963a393f1b94cc8f08bb63
slavo3dev <slavo@slavo.io>
2023-12-13 15:33:10 -0500

    improving props

---
+++ components/Alerts/ErrorAlertMsg/index.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+
+
+interface ErrorAlertMsgProps
+{
+    title: string
+}
+
+export const ErrorAlertMsg: FC<ErrorAlertMsgProps> = ( { title } ) =>
+{
+    
+
+	return (
+		<div className="py-12 bg-white">
+			<div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+				<div className="max-w-4xl mx-auto">
+					<div className="rounded-lg bg-red-50">
+						<div className="p-3">
+							<div className="flex items-start justify-between">
+								<svg className="flex-shrink-0 w-5 h-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+									<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+								</svg>
+
+								<div className="flex-1 ml-3">
+									<p className="text-sm font-bold text-red-700">{title}</p>
+									<ul className="pl-4 mt-2 space-y-1 text-sm font-medium text-red-600 list-disc list-outside">
+										<li>{title}</li>
+										<li>{title}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	);
+};
--- components/Auth/Login/index.tsx
+++ components/Auth/Login/index.tsx
@@ -1,7 +1,7 @@
 import { useState, FC } from "react";
 import supabase from "@/lib/supabase";
 import router from "next/router";
-
+import { ErrorAlertMsg } from "@/components/Alerts";
 interface LoginProps {
     signIn: () => void;
     resetPassword: () => void; 
@@ -31,103 +31,105 @@ export const LoginForm: FC<LoginProps>= ( { signIn, resetPassword} ) =>
 	};
 	
 	return (
-		<section className="py-12 bg-gray-50 sm:py-16 lg:py-20">
-			<div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-				<div className="relative max-w-md mx-auto lg:max-w-lg">
-					<div className="absolute -inset-2">
-						<div className="w-full h-full mx-auto rounded-3xl opacity-30 blur-lg filter"></div>
-					</div>
+		<>
+			{ signInError && <ErrorAlertMsg title={signInError} /> }
+			<section className="py-12 bg-gray-50 sm:py-16 lg:py-20">
+				<div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div className="relative max-w-md mx-auto lg:max-w-lg">
+						<div className="absolute -inset-2">
+							<div className="w-full h-full mx-auto rounded-3xl opacity-30 blur-lg filter"></div>
+						</div>
 
-					<div className="relative overflow-hidden bg-white shadow-xl rounded-xl">
-						<div className="px-4 py-6 sm:px-8">
-							<div className="flex items-center justify-between">
-								<h1 className="text-xl font-bold text-gray-900 font-pj">Sign in</h1>
+						<div className="relative overflow-hidden bg-white shadow-xl rounded-xl">
+							<div className="px-4 py-6 sm:px-8">
+								<div className="flex items-center justify-between">
+									<h1 className="text-xl font-bold text-gray-900 font-pj">Sign in</h1>
 
-								<p className="text-base font-normal text-gray-900 font-pj">Don’t have an account? <a onClick={() => signIn()} title="" className="font-bold rounded hover:underline focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2">Join now</a></p>
-							</div>
+									<p className="text-base font-normal text-gray-900 font-pj">Don’t have an account? <a onClick={() => signIn()} title="" className="font-bold rounded hover:underline focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2">Join now</a></p>
+								</div>
 
-							<form action="#" method="POST" className="mt-12">
-								<div className="space-y-4">
-									<div>
-										<label  className="text-base font-medium text-gray-900 font-pj"> Email </label>
-										<div className="mt-2.5">
-											<input
-												onChange={(e) => {
-													setUserEmail(e.target.value);
-												}}
-												type="email" name="" id="" placeholder="Email address" className="block w-full px-4 py-4 text-gray-900 placeholder-gray-600 bg-white border border-gray-400 rounded-xl focus:border-gray-900 focus:ring-gray-900 caret-gray-900" />
+								<form action="#" method="POST" className="mt-12">
+									<div className="space-y-4">
+										<div>
+											<label  className="text-base font-medium text-gray-900 font-pj"> Email </label>
+											<div className="mt-2.5">
+												<input
+													onChange={(e) => {
+														setUserEmail(e.target.value);
+													}}
+													type="email" name="" id="" placeholder="Email address" className="block w-full px-4 py-4 text-gray-900 placeholder-gray-600 bg-white border border-gray-400 rounded-xl focus:border-gray-900 focus:ring-gray-900 caret-gray-900" />
+											</div>
 										</div>
-									</div>
 
-									<div>
-										<div className="flex items-center justify-between">
-											<label  className="text-base font-medium text-gray-900 font-pj"> Password </label>
+										<div>
+											<div className="flex items-center justify-between">
+												<label  className="text-base font-medium text-gray-900 font-pj"> Password </label>
 
-											<a onClick={() => resetPassword()} className="text-base font-medium text-gray-500 rounded font-pj hover:text-gray-900 hover:underline focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Forgot Password? </a>
+												<a onClick={() => resetPassword()} className="text-base font-medium text-gray-500 rounded font-pj hover:text-gray-900 hover:underline focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Forgot Password? </a>
+											</div>
+											<div className="mt-2.5">
+												<input
+													onChange={(e) => {
+														setUserPassword(e.target.value);
+													}}
+													type="password" name="" id="" placeholder="Password (min. 8 character)" className="block w-full px-4 py-4 text-gray-900 placeholder-gray-600 bg-white border border-gray-400 rounded-xl focus:border-gray-900 focus:ring-gray-900 caret-gray-900" />
+											</div>
 										</div>
-										<div className="mt-2.5">
-											<input
-												onChange={(e) => {
-													setUserPassword(e.target.value);
-												}}
-												type="password" name="" id="" placeholder="Password (min. 8 character)" className="block w-full px-4 py-4 text-gray-900 placeholder-gray-600 bg-white border border-gray-400 rounded-xl focus:border-gray-900 focus:ring-gray-900 caret-gray-900" />
-										</div>
-									</div>
-
-									<div className="relative flex items-center mt-4">
+  
+										{/* <div className="relative flex items-center mt-4">
 										<div className="flex items-center h-5">
 											<input type="checkbox" name="terms" id="terms" className="w-5 h-5 text-gray-900 border-gray-300 rounded focus:ring-gray-900" />
 										</div>
 
-										{/* <div className="ml-3 text-base">
+										<div className="ml-3 text-base">
 											<label className="font-normal text-gray-900 font-pj"> Remember me </label>
-										</div> */}
+										</div>
+									</div> */}
 									</div>
-								</div>
 
-								<button
-									onClick={(e) => {
-										e.preventDefault();
-										signInWithEmail();
-									}}
-									type="submit"
-									className="flex items-center justify-center w-full px-8 py-4 mt-5 text-base font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-pj hover:bg-gray-600"
-								>
+									<button
+										onClick={(e) => {
+											e.preventDefault();
+											signInWithEmail();
+										}}
+										type="submit"
+										className="flex items-center justify-center w-full px-8 py-4 mt-5 text-base font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-pj hover:bg-gray-600"
+									>
                             Sign in
-								</button>
-							</form>
+									</button>
+								</form>
 
-							<svg className="w-auto h-4 mx-auto mt-8 text-gray-300" viewBox="0 0 172 16" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 11 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 46 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 81 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 116 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 151 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 18 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 53 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 88 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 123 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 158 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 25 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 60 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 95 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 130 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 165 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 32 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 67 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 102 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 137 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 172 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 39 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 74 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 109 1)" />
-								<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 144 1)" />
-							</svg>
+								<svg className="w-auto h-4 mx-auto mt-8 text-gray-300" viewBox="0 0 172 16" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 11 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 46 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 81 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 116 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 151 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 18 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 53 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 88 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 123 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 158 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 25 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 60 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 95 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 130 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 165 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 32 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 67 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 102 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 137 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 172 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 39 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 74 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 109 1)" />
+									<line y1="-0.5" x2="18.0278" y2="-0.5" transform="matrix(-0.5547 0.83205 0.83205 0.5547 144 1)" />
+								</svg>
 
-							<a
-								href="#"
-								title=""
-								className="
+								<a
+									href="#"
+									title=""
+									className="
                             flex
                             items-center
                             justify-center
@@ -146,14 +148,14 @@ export const LoginForm: FC<LoginProps>= ( { signIn, resetPassword} ) =>
                             hover:bg-gray-200
                             focus:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200
                             font-pj"
-								role="button" onClick={() => router.push("/auth/login")}>
+									role="button" onClick={() => router.push("/auth/login")}>
 											Login in with Github
-							</a>
+								</a>
+							</div>
 						</div>
-						{signInError}
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 };
