addbd7b068b7b9f02946c20e8dc3867032c00e57
slavo3dev <slavo@slavo.io>
2023-12-29 10:08:56 -0500

    fixing reset password

--- components/Auth/ResetPassword/index.tsx
+++ components/Auth/ResetPassword/index.tsx
@@ -70,7 +70,7 @@ export const ResetPassword: FC<ResetPasswordProps> = ( { resetPassword } ) =>
 									{ errorMsg }
 									<div>
 										<button type="submit" className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
-											onClick={ handlePasswordReset }>
+											onClick={ async () => { await handlePasswordReset(); } }>
                                             Reset Password
 										</button>
 									</div>
