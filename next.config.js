72178a468a35d6a09ca06564931ec387e1586f1f
Slavoljub Popovic <18499909+slavo3dev@users.noreply.github.com>
2024-06-07 14:31:22 -0400

    creating pwa app (#57)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable indent */
 /* eslint-disable no-undef */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
