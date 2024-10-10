404b1e9a31226c887a8b53b1ad2a9961db60eb62
SdZika <markozivkovic1982@gmail.com>
2024-10-10 20:45:42 +0200

    add custom hook useLocalStorage

--- components/Categorysearch/index.tsx
+++ components/Categorysearch/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { FC } from "react";
 import classes from "./category-search.module.css";
 import { useRouter } from "next/router";
+import { useLocalStorage } from "@/lib/hooks/useLocalStorage";
 
 interface Props {
     onSearch: ( param?: any ) => void,
@@ -14,29 +15,18 @@ export const CategorySearch: FC<Props> = ( { onSearch, posts }: Props ) =>
     
 	const [ selectedOption, setSelectedOption ] = useState("");
     
-	const categories: [ string ] = posts.map( ( post: any ) => post.category ).sort().filter( ( item: string, index: number, arr: [] ) => { return !index || item != arr[ index - 1 ]; } );
+	const categories: [string] = posts.map( ( post: any ) => post.category ).sort().filter( ( item: string, index: number, arr: [] ) => { return !index || item != arr[ index - 1 ]; } );
 	categories.unshift("ALL");
-    
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			const storedValue = localStorage.getItem("selectedOption");
-			if (storedValue) {
-				setSelectedOption(storedValue);
-			}
-		}
-	}, []);
 
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			localStorage.setItem("selectedOption", selectedOption);
-		}
-	}, [selectedOption]);
+	useLocalStorage({ value: selectedOption, setValue: setSelectedOption, key: "selectedOption" });
 
 	function submitHandler(event: any) {
 		event.preventDefault();
+		console.log("hERE WILL NEED TO SEE IF EVENT CHANGE", event.target.value);
 		setSelectedOption( event.target.value );
+		
 		const categorySlug = event.target.value.toLowerCase().replace( " ", "-" );
-		event.target.value === "ALL" && localStorage.setItem("selectedOption", "ALL");
+		
 		event.target.value === "ALL" ? router.push( "/blog" ) : onSearch(categorySlug);	
 	}
     
@@ -51,5 +41,7 @@ export const CategorySearch: FC<Props> = ( { onSearch, posts }: Props ) =>
 				</div>
 			</div>
 		</form>
+	
+	
 	);
 };
