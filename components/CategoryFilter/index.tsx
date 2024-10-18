f02db5ea5565f77b8bd4af2070703473e0e75b3d
Natasa90 <154846657+Natasa90@users.noreply.github.com>
2024-10-18 14:40:14 -0500

    refactored menu items in Free Resources (#67)
    Co-authored-by: Natasa Stefanovic <natasa@Natasas-MacBook-Air.local>

--- components/CategoryFilter/index.tsx
+++ components/CategoryFilter/index.tsx
@@ -1,27 +1,44 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { CATEGORIES } from "@/lib/constants";
+import { useLocalStorage } from "@/lib/hooks/useLocalStorage";
 
 
 export const CategoryFilter: FC<any> = ({ setCurrentCategory }) => {
+
+	const [selectedResCat, setSelectedResCat] = useState<string>("");
+	
+	useLocalStorage({ value: selectedResCat, setValue: setSelectedResCat, key: "selectedResCat" });
+	
+
+	useEffect(() => {
+
+        if (selectedResCat) {
+            setCurrentCategory(selectedResCat); // Search based on the initially stored active category
+        }
+    }, [selectedResCat, setCurrentCategory]);
+
+	const handleResClick = (resCat: string) => {
+        setSelectedResCat(resCat); 
+        setCurrentCategory(resCat); 
+    };
+
 	return (
-		<aside>
-			<ul className="flex w-full">
-				<button
-					className="hover:bg-blue-100 bg-blue-500 text-white font-bold py-2 px-4 mt-3 rounded m-3"
-					onClick={() => setCurrentCategory("all")}
-				>
-            All
-				</button>
-				{CATEGORIES.map((cat: any) => (
-					<button
-						key={cat.name}
-						className="hover:bg-blue-100 bg-blue-500 text-white font-bold py-2 px-4 mt-3 rounded m-3"
-						onClick={() => setCurrentCategory(cat.name)}
-					>
-						{cat.name}
-					</button>
-				))}
-			</ul>
-		</aside>
-	);
-};
+	    <aside className="flex flex-wrap gap-5 mt-8 justify-center">
+        {CATEGORIES.map((cat: any, index: number) => {
+			const isActive = selectedResCat
+				? selectedResCat.toLocaleLowerCase() === cat.name.toLocaleLowerCase()
+				: index === 0; 
+			return (
+            <span
+                key={cat.name}
+                onClick={() => handleResClick(cat.name)}
+                className={`text-xs font-semibold text-indigo-600 border border-indigo-300 rounded-full inline-flex items-center px-2.5 py-1 hover:text-red-600 cursor-pointer 
+                    ${isActive ? "bg-blue-500 text-white" : "bg-indigo-50"}`}
+            >
+                {cat.name === "all" ? "ALL" : cat.name}
+            </span>
+			);
+		})}
+    </aside>
+)
+}
