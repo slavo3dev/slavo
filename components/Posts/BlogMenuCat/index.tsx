3da837c23e9c7b18da145e2dd5a5163cd936316f
Natasa90 <natasa.golubovic90@gmail.com>
2024-10-20 13:56:45 -0500

    got back commented code

--- components/Posts/BlogMenuCat/index.tsx
+++ components/Posts/BlogMenuCat/index.tsx
@@ -1,9 +1,9 @@
-import { FC, useEffect } from "react";
 import { useCategory } from "@/lib/hooks/useCategory";
+import { FC, useEffect } from "react";
 
 interface Props {
     categories: string[];
-    onSearch: (param?: string) => void;
+    onSearch: ( param?: string ) => void
 }
 
 export const BlogMenuCat: FC<Props> = ({ categories, onSearch }) => {
@@ -31,3 +31,17 @@ export const BlogMenuCat: FC<Props> = ({ categories, onSearch }) => {
         </div>
     );
 };
+
+/* 
+    <span className="text-xs font-semibold text-gray-900 bg-gray-50 border border-gray-300 rounded-full inline-flex items-center px-2.5 py-1"> Normal </span>
+
+    <span className="text-xs font-semibold text-indigo-600 bg-indigo-50 border border-indigo-300 rounded-full inline-flex items-center px-2.5 py-1"> Regular </span>
+
+    <span className="text-xs font-semibold text-green-600 bg-green-50 border border-green-300 rounded-full inline-flex items-center px-2.5 py-1"> Positive </span>
+
+    <span className="text-xs font-semibold text-yellow-600 bg-yellow-50 border border-yellow-300 rounded-full inline-flex items-center px-2.5 py-1"> New </span>
+
+    <span className="text-xs font-semibold text-red-600 bg-red-50 border border-red-300 rounded-full inline-flex items-center px-2.5 py-1"> Important </span>
+
+    <span className="text-xs font-semibold text-indigo-600 bg-indigo-50 border border-indigo-300 rounded-full inline-flex items-center px-1.5 py-1"> 18 </span>
+*/
