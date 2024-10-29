586d6951ff2094390e3104f142b9f8e876b01514
Natasa90 <natasa.golubovic90@gmail.com>
2024-10-29 11:17:24 -0500

    added submit button and modal for mobile view

--- components/Subscribe/subscribeButton.tsx
+++ components/Subscribe/subscribeButton.tsx
@@ -2,9 +2,10 @@ import { FC } from "react";
 
 interface SubscribeButtonProps {
   setIsModalOpen: (isOpen: boolean) => void;
+  className?: string;
 }
 
-export const SubscribeButton: FC<SubscribeButtonProps> = ({ setIsModalOpen }) => {
+export const SubscribeButton: FC<SubscribeButtonProps> = ({ setIsModalOpen, className }) => {
   const handleClick = () => {
     localStorage.setItem("subscribeOpen",JSON.stringify(true));
     setIsModalOpen(true);
@@ -12,7 +13,7 @@ export const SubscribeButton: FC<SubscribeButtonProps> = ({ setIsModalOpen }) =>
 
   return (
         <button
-        className="py-2 px-4 text-white bg-blue-500 rounded hover:bg-blue-700 transition"
+        className="py-1 px-2 text-white bg-blue-400 rounded hover:bg-blue-700 transition md:py-2 md:px-4"
         onClick={handleClick}
         >
         Subscribe Now
