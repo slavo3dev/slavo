ff18a6156ce0dd73248d0b46a1e111fa3ab092ac
Natasa90 <natasa.golubovic90@gmail.com>
2024-11-29 10:17:29 -0600

    style left for updating, functionality done

--- components/DropDown/index.tsx
+++ components/DropDown/index.tsx
@@ -10,7 +10,6 @@ export const DropDown: FC<DropDownProps> = ({ label, items, onItemClick }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const dropdownRef = useRef<HTMLDivElement>(null);
 
-    // Close the dropdown when clicking outside
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (
@@ -27,33 +26,20 @@ export const DropDown: FC<DropDownProps> = ({ label, items, onItemClick }) => {
     }, []);
 
     const handleClick = (item: string) => {
-        onItemClick(item);  // Call the onItemClick handler passed from parent
+        onItemClick(item); 
     };
 
     return (
         <div ref={dropdownRef} style={{ position: 'relative', display: 'inline-block' }}>
             <button
-                onClick={() => setIsOpen((prev) => !prev)} // Toggle dropdown visibility
+                onClick={() => setIsOpen((prev) => !prev)} 
                 aria-haspopup="true"
                 aria-expanded={isOpen}
             >
                 {label}
             </button>
             {isOpen && (
-                <ul
-                    style={{
-                        position: 'absolute',
-                        top: '100%',
-                        left: 0,
-                        background: 'white',
-                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                        listStyle: 'none',
-                        padding: '0',
-                        margin: '0',
-                        zIndex: 10,
-                        width: '200px', // Optional: set a fixed width to control the column size
-                    }}
-                >
+              <ul className="absolute left-0 mt-2 bg-white shadow-xl w-48 z-50 flex flex-col rounded-lg border border-gray-300">
                     {items.map((item, index) => (
                         <li
                             key={index}
--- components/Posts/BlogDropDown/index.tsx
+++ components/Posts/BlogDropDown/index.tsx
@@ -1,17 +1,28 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { DropDown } from '@/components/DropDown'
 import { useCategoryHook } from '@/lib/hooks/useCategoryHook'
 import { CatProps } from "../BlogMenuCat"
 
 export const BlogDropDown: FC<CatProps> = ({ categories, onSearch }) => {
 
-
+    const router = useRouter()
     const { activeCategory, handleCategoryClick } = useCategoryHook(categories, "selectedCategory");
+    const [selectedOption, setSelectedOption] = useState(activeCategory);
 
     // Handler to manage category selection and trigger onSearch
+     useEffect(() => {
+        setSelectedOption(activeCategory);
+    }, [activeCategory]);
+
     const handleItemClick = (category: string) => {
-        handleCategoryClick(category); // Updates the active category
-        onSearch(category); // Triggers the search with the selected category
+        setSelectedOption(category); // Update the selected option state
+        handleCategoryClick(category); // Update the active category in the hook
+        
+        const categorySlug = category.toLowerCase().replace(" ", "-");
+        category === "ALL" 
+            ? router.push("/blog")  // Redirect to the blog page if "ALL" is selected
+            : onSearch(categorySlug); // Trigger search for a specific category
     };
 
     return (
