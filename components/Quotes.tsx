e74bc45b65829bedbc99f3a2ed91863cf7389f94
SdZika <markozivkovic1982@gmail.com>
2024-11-29 12:26:04 +0100

    add api for quotes

--- components/Quotes.tsx
+++ components/Quotes.tsx
@@ -1,44 +1,56 @@
 import { useState, useEffect } from "react";
 
-const quotes = [
-  { id: 1,
-    text: "Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come.",
-    author: "Dwayne Johnson",
-  },
-  {
-    id: 2,
-    text: "Opportunities don't happen, you create them.",
-    author: "Chris Grosser",
-  },
-  {
-    id: 3,
-    text: "Don't watch the clock; do what it does. Keep going.",
-    author: "Sam Levenson",
-  },
-  {
-    id: 4,
-    text: "Success usually comes to those who are too busy to be looking for it.",
-    author: "Henry David Thoreau",
-  },
-];
-
 export const Quotes = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentQuote, setCurrentQuote] = useState<{ quote: string; author: string } | null>(null);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % quotes.length);
-    }, 7000);
+    
+    const getQuote = async  () => {
 
-    return () => clearInterval(intervalId);
-  }, []);
+      const apiKey = process.env.NEXT_PUBLIC_NET_NINJA_API_KEY;
 
-  const currentQuote = quotes[currentIndex];
+      if (!apiKey) {
+        console.error('API key is not defined. Please set the NEXT_PUBLIC_NET_NINJA_API_KEY environment variable.');
+        return;
+      }
+
+      try {
+        const response = await fetch("https://api.api-ninjas.com/v1/quotes?category=computers", {
+          method: 'GET',
+          headers: {
+            'x-api-key': apiKey, 
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch the quote');
+        }
+
+        const data = await response.json();
+        if (Array.isArray(data) && data.length > 0) {
+          setCurrentQuote(data[0]);
+        } else {
+          console.error('No quotes found or data is not in expected format');
+        }
+      } catch (error) {
+        console.error('Error fetching quote:', error);
+      }
+    };
+
+    getQuote()
+
+  }, [])
 
   return (
-    <div key={currentQuote.id} className="w-full flex flex-col justify-center mb-6">
-      <h5 className="font-extralight text-center text-lg italic">{currentQuote.text}</h5>
-      <p className="text-center font-bold italic">{currentQuote.author}</p>
+    <div className="w-full flex flex-col justify-center mb-6">
+         {currentQuote ? (
+        <>
+          <h5 className="font-extralight text-center text-lg italic">{currentQuote.quote}</h5>
+          <p className="text-center font-bold italic">{currentQuote.author}</p>
+        </>
+      ) : (
+        <p className="text-center">Loading...</p>
+      )}
     </div>
   );
 };
