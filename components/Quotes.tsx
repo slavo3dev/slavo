18eb5317c9c4a78f9423ca7d7893bce2fab0f177
Natasa90 <natasa.golubovic90@gmail.com>
2024-12-05 14:54:38 +0100

    Quotes chnages

--- components/Quotes.tsx
+++ components/Quotes.tsx
@@ -1,35 +1,37 @@
 import { useState, useEffect } from "react";
 
 export const QuoteFetcher = () => {
-  const [currentQuote, setCurrentQuote] = useState<string | null>("null");
+  const [currentQuote, setCurrentQuote] = useState<string | null>(null); 
   const [author, setAuthor] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);  
 
   const getQuote = async () => {
     try {
-      const response = await fetch("/api/get-quotes"); 
+      const response = await fetch("/api/get-quotes");
 
       if (!response.ok) {
         throw new Error("Failed to fetch quote");
       }
 
-      const data = await response.json(); 
+      const data = await response.json();
 
       if (data && data.quote && data.author) {
         setCurrentQuote(data.quote);
         setAuthor(data.author);
-        setError(null); 
+        setError(null);
       } else {
         setError("No quote found");
       }
     } catch (error) {
       setError(`Error fetching quote: ${(error as Error).message}`);
       console.error("Error fetching quote:", error);
+    } finally {
+      setLoading(false);  
     }
   };
 
   useEffect(() => {
-
     getQuote();
 
     const interval = setInterval(() => {
@@ -41,8 +43,10 @@ export const QuoteFetcher = () => {
 
   return (
     <div className="w-full flex flex-col justify-center mb-6">
-      {error ? (
-        <p className="text-center text-red-500">{error}</p>
+      {loading ? (
+        <p>Loading...</p> /* Show loading message while fetching */
+      ) : error ? (
+        <p className="text-center text-red-500">{error}</p>  /* Show error if any */
       ) : (
         <div className="text-center">
           <h5 className="font-extralight text-lg italic">{`"${currentQuote}"`}</h5>
