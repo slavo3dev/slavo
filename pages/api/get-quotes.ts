b31fcf660017d323f4dc187a48c5b4cb7b220f55
Milutin25 <152893450+Milutin25@users.noreply.github.com>
2025-01-05 01:08:58 +0100

    Fetched quotes from JSON file (#119)
    Co-authored-by: Milutin25 <natasa.golubovic90@gmail.com>

--- pages/api/get-quotes.ts
+++ pages/api/get-quotes.ts
@@ -1,39 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
+import { promises as fs } from 'fs';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const apiUrl = process.env.QUOTES_API_URL; 
-  const apiKey = process.env.API_NINJAS_API_KEY;
-
-  if (!apiUrl || !apiKey) {
-    console.error('API URL or API Key is not defined');
-    return res.status(500).json({ error: 'API URL or API Key is not defined in .env file' });
-  }
-
   try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'x-api-key': apiKey, 
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch the quote');
-    }
-
-    const data = await response.json(); 
+    const filePath = path.join(process.cwd(), 'data', 'stoic-quotes.json');
+    const fileContents = await fs.readFile(filePath, 'utf8');
+    const quotes = JSON.parse(fileContents);
     
-    if (Array.isArray(data) && data.length > 0) {
-      const { quote, author } = data[0]; 
-      res.status(200).json({ quote, author }); 
+    if (Array.isArray(quotes) && quotes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const { quote, author } = quotes[randomIndex];
+
+      res.status(200).json({ quote, author });
     } else {
       res.status(400).json({ error: 'No quotes found in the response data' });
     }
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ error: `Failed to fetch quote: ${error.message}` });
-    } else {
-      res.status(500).json({ error: 'An unknown error occurred' });
-    }
+    console.error('Error reading JSON file:', error);
+    res.status(500).json({ error: 'Failed to fetch quotes from JSON file' });
   }
 };
 
