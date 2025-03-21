3c9397c6715d89c1a846d50e44a84c1a66bf8d3c
Natasa90 <154846657+Natasa90@users.noreply.github.com>
2025-03-21 15:11:15 -0500

    Fixed the Search Category Bar  (#145)
    * Fixed the Search Category Bar to match selected Category from the Drop Down Menu
    
    * Removed func from line 17
    
    * comments removed

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,15 +1,27 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextPage } from "next";
-import { BlogPosts } from "@/components/index";
+import { BlogPosts, CategorySearch } from "@/components/index";
 import { getAllPosts } from "lib/posts-lib";
+import { useRouter } from "next/router";
 
 
 
 
 const blog: NextPage = ( { posts }: any ) => {
 
+  const router = useRouter();
+
+	function findCategoryHandle ( category: string ) {
+		const fullPath = category
+			? `/category/${category}`
+			: "/blog"; 
+		router.push( fullPath );
+	}
+
 	return (
-		<BlogPosts posts={posts} />
+    <>
+      <CategorySearch onSearch={ findCategoryHandle } posts={ posts } />
+		  <BlogPosts posts={posts} />
+    </>
 	);
 };
 
