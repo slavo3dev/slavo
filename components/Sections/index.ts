51342ad1e7c01f13c7c75b2a9b7d928ee38c35f7
Natasa90 <natasa.golubovic90@gmail.com>
2025-03-25 11:08:35 -0500

    Created 2 components - ProgramsInfoSection, CounterSection and removed Subscribe component

--- components/Sections/index.ts
+++ components/Sections/index.ts
@@ -3,5 +3,7 @@ import { Solution } from "./Solution";
 import { CounterUp } from "./CounterUp";
 import { Breadcrumb} from "./Breadcrumb";
 import { PricingSection } from "./PricingSection";
+import { ProgramsInfoSection } from "./ProgramsInfoSection";
+import { CounterSection } from "./CounterSection";
 
-export { Growth, Solution, CounterUp, Breadcrumb, PricingSection };
+export { Growth, Solution, CounterUp, Breadcrumb, PricingSection, ProgramsInfoSection, CounterSection };
--- components/index.ts
+++ components/index.ts
@@ -14,7 +14,7 @@ import { MentorAI } from "./MentorAI";
 import { NewResourceFrom } from "./Forms";
 import { FreeSourcesList } from "./FreeSourcesList";
 import { VideoCard, VideoLine } from "./VideoElements";
-import { Breadcrumb, Growth, Solution, CounterUp, PricingSection } from "./Sections";
+import { Breadcrumb, Growth, Solution, CounterUp, PricingSection, ProgramsInfoSection, CounterSection } from "./Sections";
 import { LinkEvents } from "./Primitives";
 import { SignUp } from "./Auth/SingUp";
 import { SignIn } from "./Auth/SignIn";
@@ -23,4 +23,4 @@ import { Preloader } from "./Elements";
 
 
 
-export {Layout, LearningSources, FeaturedPosts, HeadBasePage, MainNavigation, BlogPosts, PostContent, PostHeader, ContactForm, Subscribe, Footer,  CategorySearch, MentorAI, NewResourceFrom, FreeSourcesList, VideoCard, VideoLine, Breadcrumb, CounterUp, Growth, Solution, Hero, LinkEvents, SignIn, SignUp, PorchList, PorchHeader, PorchForm, Preloader, PricingSection };
+export {Layout, LearningSources, FeaturedPosts, HeadBasePage, MainNavigation, BlogPosts, PostContent, PostHeader, ContactForm, Subscribe, Footer,  CategorySearch, MentorAI, NewResourceFrom, FreeSourcesList, VideoCard, VideoLine, Breadcrumb, CounterUp, Growth, Solution, Hero, LinkEvents, SignIn, SignUp, PorchList, PorchHeader, PorchForm, Preloader, PricingSection, ProgramsInfoSection, CounterSection };
