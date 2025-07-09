13dcf999f0071b8013853930de5d02486aeb6b90
SdZika <markozivkovic1982@gmail.com>
2025-07-09 21:55:40 +0200

    update stripe api version

--- lib/stripe/index.ts
+++ lib/stripe/index.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
 
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2025-03-31.basil"});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2025-05-28.basil"});
 
