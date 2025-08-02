495e1484c323ce58fe264a4dc609b1a46a648460
SdZika <markozivkovic1982@gmail.com>
2025-08-02 20:41:55 +0200

    build files refactored

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+
+import * as React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
+}
